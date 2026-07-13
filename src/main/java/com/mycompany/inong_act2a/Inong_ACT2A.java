5ba53e05efd9f0eb9b6ef35455fb0fac46cb3e87
CL2-PC <CL2-PC@CL2-PC27>
2026-07-14 07:35:57 +0800

    rps

---
+++ src/main/java/com/mycompany/inong_act2a/Inong_ACT2A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inong_act2a;
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class Inong_ACT2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
